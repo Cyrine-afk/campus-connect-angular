24a748e92e0959ca56c02f5d00098a189c024960
Cyrine-afk <cyrine.trabelsi@esprit.tn>
2023-05-05 19:04:13 +0100

    Update edit component

--- src/app/edit-fournisseur/edit-fournisseur.component.ts
+++ src/app/edit-fournisseur/edit-fournisseur.component.ts
@@ -12,34 +12,34 @@ import { FournisseurService } from '../services/fournisseur/fournisseur.service'
 export class EditFournisseurComponent implements OnInit {
 
   /*to read the id parameter*/
-  idFourn:any ;
-  showAlert = false;
-  fournisseur = new Fournisseur();
+  idFourn:any 
+  showAlert = false
+  fournisseur = new Fournisseur()
   
   constructor(private route:ActivatedRoute, private fournisseurService:FournisseurService) { } /*dependecy used to read the id parameter*/
   
   
   
   ngOnInit(): void {
-    this.idFourn = this.route.snapshot.params['idFourn'];
+    this.idFourn = this.route.snapshot.params['idFourn']
 
     this.fournisseurService.getFournisseur(this.idFourn).subscribe((response: any) => {
-        this.fournisseur = response;
-        console.log(response);
+        this.fournisseur = response
+        console.log(response)
         
-      });
+      })
   }
 
   editFournisseur() {
     this.fournisseurService.editFournisseur(this.fournisseur,this.idFourn).subscribe((response) => {
-      this.showAlert = true;
-      console.log(response);
+      this.showAlert = true
+      console.log(response)
     });
 
   
   }
   closeAlert() {
-    this.showAlert = false;
+    this.showAlert = false
   }
 
 }
